77db57197ab446ae3f18ab545859741ac22514e4
Manfredi Piraino <manfredipiraino@gmail.com>
2024-03-29 22:39:50 +0100

    refactor: new dynamic-container for dynamic generation wip

---
+++ src/app/dynamic-container/dynamic-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './dynamic-container.component.html',
+  styleUrl: './dynamic-container.component.scss'
+})
+export class DynamicContainerComponent {
+
+}
