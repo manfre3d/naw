51315198e82835f982352c3afabf882ade4e2456
Manfredi Piraino <manfredipiraino@gmail.com>
2024-04-12 23:31:54 +0200

    add: wip about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DynamicContainerComponent } from '../dynamic-container/dynamic-container.component';
+import { DynamicCardComponent } from "../dynamic-card/dynamic-card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+    selector: 'app-about',
+    standalone: true,
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.scss',
+    imports: [
+        DynamicContainerComponent,
+        DynamicCardComponent,
+        CommonModule
+    ]
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+  @Input() subDescriptor:any;
+  test: any =[{}]
+
+  ngOnInit(): void {
+    console.log("ABOUT INIT");
+    console.log(this.subDescriptor);
+  }
+
 
 }
