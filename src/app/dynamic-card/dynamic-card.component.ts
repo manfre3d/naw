2a6a6138c869ecae6cc70640ca284546df6c4220
Manfredi Piraino <manfredipiraino@gmail.com>
2024-04-12 23:29:47 +0200

    add: dynamic card component

---
+++ src/app/dynamic-card/dynamic-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './dynamic-card.component.html',
+  styleUrl: './dynamic-card.component.scss'
+})
+export class DynamicCardComponent {
+
+}
