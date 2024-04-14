a5c407c9777724eee0e93ac19d9ac7624a32691e
Manfredi Piraino <manfredipiraino@gmail.com>
2024-04-14 23:27:59 +0200

    add: secondary title + style refactor

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,15 +1,16 @@
-import { DOCUMENT } from '@angular/common';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { AfterViewInit, Component, Inject, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent implements OnInit, AfterViewInit{
-  dynamicText: any;
+  secondaryTextActive: boolean=false;
+
   @Input() subDescriptor:any;
   
   constructor(@Inject(DOCUMENT) private document: Document){}
@@ -21,8 +22,9 @@ export class HeroComponent implements OnInit, AfterViewInit{
     // js automation for dynamic text
     let welcomeTitle = this.document.getElementById("dynamic_introduction") as HTMLElement
     welcomeTitle.addEventListener("animationend", ()=>{
-      welcomeTitle.classList.remove("typing-animation");
+      welcomeTitle.classList.remove("typing-animation-primary");
       welcomeTitle.style.border="";
+      this.secondaryTextActive=true;
     });
 
   }
