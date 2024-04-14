29ee0c6e8aeb527e43f1ec0688998fcdda837ef4
Manfredi Piraino <manfredipiraino@gmail.com>
2024-04-14 23:30:43 +0200

    add: dynamic obj for card content + dynamic component generation + style refactor

--- src/app/dynamic-card/dynamic-card.component.ts
+++ src/app/dynamic-card/dynamic-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-card',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './dynamic-card.component.scss'
 })
 export class DynamicCardComponent {
-
+  @Input() cardContent:any;
 }
