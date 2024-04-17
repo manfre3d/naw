6493060c2458d3e7f56a29dc9a52560741e757e0
Manfredi Piraino <manfredipiraino@gmail.com>
2024-04-17 23:16:32 +0200

    add: basic contacts component wip

--- src/app/dynamic-single-container/dynamic-single-container.component.ts
+++ src/app/dynamic-single-container/dynamic-single-container.component.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { AboutComponent } from '../about/about.component';
 import { DynamicCardComponent } from '../dynamic-card/dynamic-card.component';
+import { ContactsComponent } from '../contacts/contacts.component';
 
 @Component({
   selector: 'app-dynamic-single-container',
@@ -15,7 +16,8 @@ import { DynamicCardComponent } from '../dynamic-card/dynamic-card.component';
     FooterComponent,
     HeaderComponent,
     DynamicCardComponent,
-    AboutComponent
+    AboutComponent,
+    ContactsComponent
   ],
   templateUrl: './dynamic-single-container.component.html',
   styleUrl: './dynamic-single-container.component.scss'
