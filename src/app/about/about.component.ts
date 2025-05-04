82ef6920b30d957eed2cf2259a335535bba5f63d
Manfredi Piraino <manfredipiraino@gmail.com>
2025-05-04 16:10:43 +0200

    test

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,25 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DynamicCardComponent } from "../dynamic-card/dynamic-card.component";
+import { DynamicCardComponent } from '../dynamic-card/dynamic-card.component';
 import { SharedModule } from '../shared/shared.module';
 
 @Component({
-    selector: 'app-about',
-    standalone: true,
-    templateUrl: './about.component.html',
-    styleUrl: './about.component.scss',
-    imports: [
-        DynamicCardComponent,
-        SharedModule
-    ]
+  selector: 'app-about',
+  standalone: true,
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+  imports: [DynamicCardComponent, SharedModule],
 })
-export class AboutComponent implements OnInit{
-  @Input() subDescriptor:any;
-  test: any =[{}]
+export class AboutComponent implements OnInit {
+  @Input() subDescriptor: any;
+  test: any = [{}];
 
   ngOnInit(): void {
-    console.log("ABOUT INIT");
+    console.log('ABOUT INIT');
     console.log(this.subDescriptor);
   }
-
-
 }
